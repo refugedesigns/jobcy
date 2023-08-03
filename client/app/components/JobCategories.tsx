d1aa479e232f7452d8f7d57f0d4ac0097dac3617
Erasmus <refugedesigns17@gmail.com>
2023-08-03 11:21:36 +0000

    added skew to hero section

---
+++ client/app/components/JobCategories.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobCategories = () => {
+  return (
+    <div className='h-[500px] bg-white w-full text-black z-20'>JobCategories</div>
+  )
+}
+
+export default JobCategories
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,18 @@
 "use client"
-import Image from 'next/image'
-import HeroSection from './components/HeroSection'
+
+import { Box } from "@mui/material";
+import HeroSection from "./components/HeroSection";
+import JobCategories from "./components/JobCategories";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <HeroSection />
+      <Box className="bg-[url('/images/home/img-01.png')] bg-cover bg-center w-full min-h-[100vh] bg-[#6055f2] relative">
+        <HeroSection />
+        <Box className="bg-white w-full h-32 absolute -bottom-16 z-10 overflow-hidden -skew-y-3" />
+      </Box>
+     
+        <JobCategories />
     </main>
-  )
+  );
 }
