0acbedc1ff1d0a25e783decbd51ae9b7a7beda44
Erasmus <refugedesigns17@gmail.com>
2023-08-08 02:10:26 +0000

    completed homepage input designs

--- client/app/components/HeroSection.tsx
+++ client/app/components/HeroSection.tsx
@@ -4,10 +4,13 @@ import {
   Box,
   Button,
   FormControl,
-  TextField,
+  Input,
   Typography,
-  Container
+  Container,
+  useMediaQuery,
+  Divider
 } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import { BiBriefcaseAlt2 } from "react-icons/bi";
 
 interface Options  {
@@ -23,29 +26,38 @@ const options: readonly Options[] = [
 ]
 
 const HeroSection = () => {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up("lg"))
     const [isClearable, setIsClearable] = useState(true);
     const [isSearchable, setIsSearchable] = useState(true);
 
   return (
     <Container className="mt-40 sm:px-10 mb-20">
-      <Typography variant="h2" className="text-white text-4xl text-center max-w-md mx-auto font-bold">
+      <Typography
+        variant="h2"
+        className="text-white text-4xl text-center max-w-md mx-auto font-bold"
+      >
         Search Between More Than <span className="text-slate-400">10,000+</span>{" "}
         Open Jobs
       </Typography>
       <Typography variant="body2" className="py-4 text-center mx-auto w-full">
         Find jobs, create trackable resumes and enrich your applications
       </Typography>
-      <Box className="space-y-4 lg:flex lg:space-y-0">
-        <TextField
+      <Box className="space-y-4 lg:flex lg:space-y-0 lg:bg-whit">
+        <Input
           name="company"
           id="company"
           placeholder="Job, Company name..."
-          InputProps={{ startAdornment: <BiBriefcaseAlt2 /> }}
-          className="bg-white rounded-md lg:flex-1"
+          startAdornment={<BiBriefcaseAlt2 className="px-1 h-8 w-8" />}
+          className="bg-white rounded-md lg:flex-1 lg:rounded-tr-none lg:rounded-br-none py-3 lg:py-0"
           fullWidth
+          variant="outlined"
+          disableUnderline
+          size="medium"
         />
+        <Divider orientation="vertical" className="hidden lg:block" />
         <Select
-          className="basic-single lg:flex-1"
+          className="basic-single lg:flex-1 lg:rounded-none"
           classNamePrefix="select"
           defaultValue={"Option 1"}
           isClearable={isClearable}
@@ -61,15 +73,18 @@ const HeroSection = () => {
             }),
             container: (base) => ({
               ...base,
-              border: "1px solid gray",
+              border: matches ? "none" : "1px solid gray",
               padding: 8,
+              paddingTop: 10,
+              paddingBottom: 10,
               borderRadius: 5,
               backgroundColor: "white",
             }),
           }}
         />
+        <Divider orientation="vertical" className="hidden lg:block" />
         <Select
-          className="basic-single lg:flex-1"
+          className="basic-single lg:flex-1 lg:rounded-none"
           classNamePrefix="select"
           defaultValue={"Option 1"}
           isClearable={isClearable}
@@ -85,7 +100,7 @@ const HeroSection = () => {
             }),
             container: (base) => ({
               ...base,
-              border: "1px solid gray",
+              border: matches ? "none" : "1px solid gray",
               padding: 8,
               borderRadius: 5,
               backgroundColor: "white",
