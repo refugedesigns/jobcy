4aeb317460966d6ddc0a301502739c7c140cd2c5
Erasmus <refugedesigns17@gmail.com>
2023-08-12 06:31:17 +0000

    completed herosection design

--- client/app/components/HeroSection.tsx
+++ client/app/components/HeroSection.tsx
@@ -12,43 +12,50 @@ import {
 } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { BiBriefcaseAlt2 } from "react-icons/bi";
+import { MdOutlineDiscount } from "react-icons/md";
+import { AiOutlineBook } from "react-icons/ai";
+import { CiLocationOn } from "react-icons/ci";
+import Link from "next/link";
 
-interface Options  {
+interface Options {
   readonly value: string;
   readonly label: string;
-};
+}
 
 const options: readonly Options[] = [
-  {value: "Option 1", label: "Option 1"},
-  {value: "Option 2", label: "Option 2"},
-  {value: "Option 3", label: "Option 3"},
-  {value: "Option 4", label: "Option 4"},
-]
+  { value: "Option 1", label: "Option 1" },
+  { value: "Option 2", label: "Option 2" },
+  { value: "Option 3", label: "Option 3" },
+  { value: "Option 4", label: "Option 4" },
+];
 
 const HeroSection = () => {
-    const theme = useTheme();
-    const matches = useMediaQuery(theme.breakpoints.up("lg"))
-    const [isClearable, setIsClearable] = useState(true);
-    const [isSearchable, setIsSearchable] = useState(true);
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("lg"));
+  const [isClearable, setIsClearable] = useState(true);
+  const [isSearchable, setIsSearchable] = useState(true);
 
   return (
     <Container className="mt-40 sm:px-10 mb-20">
       <Typography
         variant="h2"
-        className="text-white text-4xl text-center max-w-md mx-auto font-bold"
+        className="text-white text-4xl text-center max-w-md mx-auto font-bold md:max-w-lg lg:text-5xl lg:max-w-2xl"
       >
         Search Between More Than <span className="text-slate-400">10,000+</span>{" "}
         Open Jobs
       </Typography>
-      <Typography variant="body2" className="py-4 text-center mx-auto w-full">
+      <Typography
+        variant="body2"
+        className="py-4 text-center mx-auto w-full md:text-lg"
+      >
         Find jobs, create trackable resumes and enrich your applications
       </Typography>
-      <Box className="space-y-4 lg:flex lg:space-y-0 lg:bg-whit">
+      <Box className="space-y-4 lg:flex lg:space-y-0 lg:bg-whit pt-10 pb-4">
         <Input
           name="company"
           id="company"
           placeholder="Job, Company name..."
-          startAdornment={<BiBriefcaseAlt2 className="px-1 h-8 w-8" />}
+          startAdornment={<BiBriefcaseAlt2 className="px-1 h-7 w-7 text-primary-blue" />}
           className="bg-white rounded-md lg:flex-1 lg:rounded-tr-none lg:rounded-br-none py-3 lg:py-0"
           fullWidth
           variant="outlined"
@@ -56,66 +63,101 @@ const HeroSection = () => {
           size="medium"
         />
         <Divider orientation="vertical" className="hidden lg:block" />
-        <Select
-          className="basic-single lg:flex-1 lg:rounded-none"
-          classNamePrefix="select"
-          defaultValue={"Option 1"}
-          isClearable={isClearable}
-          isSearchable={isSearchable}
-          name="color"
-          options={options}
-          styles={{
-            control: (provided) => ({
-              ...provided,
-              border: "none",
-              outline: "none",
-              boxShadow: "none",
-            }),
-            container: (base) => ({
-              ...base,
-              border: matches ? "none" : "1px solid gray",
-              padding: 8,
-              paddingTop: 10,
-              paddingBottom: 10,
-              borderRadius: 5,
-              backgroundColor: "white",
-            }),
-          }}
-        />
+        <Box className="relative lg:flex-1">
+          <CiLocationOn className="absolute z-10 top-1/2 transform -translate-y-1/2 h-[2.3rem] w-[2.3rem] px-2 text-primary-blue" />
+          <Select
+            className="basic-single lg:flex-1 lg:rounded-none"
+            classNamePrefix="select"
+            defaultValue={"Option 1"}
+            isClearable={isClearable}
+            isSearchable={isSearchable}
+            name="color"
+            options={options}
+            styles={{
+              control: (provided) => ({
+                ...provided,
+                border: "none",
+                outline: "none",
+                boxShadow: "none",
+                paddingLeft: 16,
+                "&:hover": {
+                  cursor: "pointer"
+                }
+              }),
+              container: (base) => ({
+                ...base,
+                border: matches ? "none" : "1px solid gray",
+                padding: 8,
+                paddingTop: 10,
+                paddingBottom: 10,
+                borderRadius: 5,
+                backgroundColor: "white",
+                "&:hover": {
+                  cursor: "pointer"
+                }
+              }),
+            }}
+          />
+        </Box>
         <Divider orientation="vertical" className="hidden lg:block" />
-        <Select
-          className="basic-single lg:flex-1 lg:rounded-none"
-          classNamePrefix="select"
-          defaultValue={"Option 1"}
-          isClearable={isClearable}
-          isSearchable={isSearchable}
-          name="color"
-          options={options}
-          styles={{
-            control: (provided) => ({
-              ...provided,
-              border: "none",
-              outline: "none",
-              boxShadow: "none",
-            }),
-            container: (base) => ({
-              ...base,
-              border: matches ? "none" : "1px solid gray",
-              padding: 8,
-              borderRadius: 5,
-              backgroundColor: "white",
-            }),
-          }}
-        />
+        <Box className="relative lg:flex-1 hover:cursor-pointer">
+          <AiOutlineBook className="absolute z-10 top-1/2 transform -translate-y-1/2 h-[2.2rem] w-[2.2rem] px-2 text-primary-blue" />
+          <Select
+            className="basic-single lg:flex-1 lg:rounded-none"
+            classNamePrefix="select"
+            defaultValue={"Option 1"}
+            isClearable={isClearable}
+            isSearchable={isSearchable}
+            name="color"
+            options={options}
+            styles={{
+              control: (provided) => ({
+                ...provided,
+                border: "none",
+                outline: "none",
+                boxShadow: "none",
+                paddingLeft: 16,
+                "&:hover": {
+                  cursor: "pointer"
+                }
+              }),
+              container: (base) => ({
+                ...base,
+                border: matches ? "none" : "1px solid gray",
+                padding: 8,
+                paddingTop: 10,
+                paddingBottom: 10,
+                borderRadius: 5,
+                backgroundColor: "white",
+                "&:hover": {
+                  cursor: "pointer"
+                }
+              }),
+            }}
+          />
+        </Box>
         <Button
           type="submit"
           variant="contained"
           disableElevation
           size="large"
-          className="bg-primary-blue  dark:text-white w-full lg:flex-1 py-4"
+          className="bg-primary-blue dark:bg-slate-800 dark:focus:bg-slate-800 dark:hover:bg-slate-700  dark:text-white w-full lg:flex-1 py-4 lg:rounded-bl-none lg:rounded-tl-none"
         >
           Find Job
         </Button>
+      </Box>
+      <Box className="flex justify-center w-full flex-wrap text-sm">
+        <Box className="flex justify-center">
+          <MdOutlineDiscount className="h-6 w-6" />
+          <Typography className="whitespace-nowrap">Trending Keywords:</Typography>
+        </Box>
+          {["Design", "Development", "Manager", "Senior"].map(
+            (keyword, index) => (
+              <Link className="mx-2" href="#" key={index}>
+                {keyword}, 
+              </Link>
+            )
+          )}
       </Box>
     </Container>
   );
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,9 +7,9 @@ import JobCategories from "./components/JobCategories";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
-      <Box className="bg-[url('/images/home/img-01.png')] bg-cover bg-bottom w-full min-h-[750px] bg-[#6055f2] relative pb-16">
+      <Box className="bg-[url('/images/home/img-01.png')] bg-cover bg-bottom w-full min-h-[750px] bg-[#6055f2] dark:bg-slate-600 relative pb-40">
         <HeroSection />
-        <Box className="bg-white w-full h-48 absolute -bottom-24 z-10 overflow-hidden -skew-y-[3deg]" />
+        <Box className="bg-white w-full h-48 absolute -bottom-28 z-10 overflow-hidden -skew-y-[2.5deg] lg:-skew-y-[4deg]" />
       </Box>
      
         <JobCategories />
