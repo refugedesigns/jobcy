b95e85bbdac4bf2e5ff486843d74f59ef3f75eec
Erasmus <refugedesigns17@gmail.com>
2023-08-18 21:30:22 +0000

    completed job-categories section

--- client/app/components/CategoryIcon.tsx
+++ client/app/components/CategoryIcon.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Box, Typography } from "@mui/material"
+import { motion } from 'framer-motion'
 
 type Props = {
     icon: React.ReactNode;
@@ -9,13 +10,13 @@ type Props = {
 
 const CategoryIcons = (props: Props) => {
   return (
-    <Box>
+    <motion.div whileHover={{ y: -10 }} transition={{duration: 0.5, ease: "easeOut"}} className="mx-auto flex flex-col items-center py-10 space-y-2 cursor-pointer">
       <Box className="w-[64px] h-[64px] bg-[rgba(118,109,244,.1)] flex justify-center items-center rounded-lg">
         {props.icon}
       </Box>
-      <Typography>{props.title}</Typography>
-      <Typography>{props.except} jobs</Typography>
-    </Box>
+      <Typography variant='body2' className='font-bold text-lg text-slate-700'>{props.title}</Typography>
+      <Typography variant='body2' className="text-slate-400">{props.except} jobs</Typography>
+    </motion.div>
   );
 }
 
--- client/app/components/SectionTitles.tsx
+++ client/app/components/SectionTitles.tsx
@@ -9,10 +9,10 @@ type Props = {
 const SectionTitles = ({title, except}: Props) => {
   return (
     <Box className="">
-        <Typography variant="h2" className="text-3xl text-center text-black max-w-md mx-auto font-bold md:max-w-lg lg:max-w-2xl">
+        <Typography variant="h2" className="text-3xl text-center text-slate-700 max-w-md mx-auto font-bold md:max-w-lg lg:max-w-2xl">
             {title}
         </Typography>
-        <Typography variant="body2" className="py-4 text-center mx-auto w-full text-black md:text-md">
+        <Typography variant="body2" className="py-4 text-center mx-auto w-full text-slate-700 md:text-md">
             {except}
         </Typography>
     </Box>
